6b86d0e4afae011f3b3371355406c0535db44fd2
Chency <chency.wang@gmail.com>
2024-09-09 19:52:09 -0700

    fixed timezone and de-sync issue

--- cal_parser.py
+++ cal_parser.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-import dateutil.tz
 import icalendar
 import pandas as pd
+
+import sys
+
+from backports.zoneinfo import ZoneInfo
 
 def parse_time_range(time_range):
     # Split the time range into start and end times
@@ -20,12 +23,50 @@ def parse_time_range(time_range):
     
     return duration
 
+try:
+    filename = sys.argv[1]
+    excel = pd.read_excel(filename)
+except:
+    print("format: python3 cal_parser.py <filename>.xlsx")
+    print("the filename should be called something like 'View_My_Courses.xlsx'.")
+    sys.exit(-1)
+
 c = icalendar.Calendar()
-excel = pd.read_excel("test.xlsx")
+c.add('version', 2.0)
+c.add('proid', "myCalender")
+
 excel = excel.iloc[2:, :]
 excel.reset_index(drop=True)
 
-courses = [] # each course has a: course code & number, type, days, time, location, start, and end
+c = icalendar.Calendar()
+c.add('version', 2.0)
+c.add('proid', "myCalender")
+
+#add timezone support, why does daylights saving exists, suffering
+tz = ZoneInfo("America/Vancouver")
+timezone = icalendar.Timezone()
+timezone.add('TZID', 'America/Vancouver')
+
+standard = icalendar.TimezoneStandard()
+standard.add('DTSTART', datetime(1970, 11, 1, 2, 0, 0))
+standard['TZOFFSETFROM'] = '-0700'
+standard['TZOFFSETTO']= '-0800'
+standard.add('TZNAME', 'PST')
+standard.add('RRULE', {'FREQ': 'YEARLY', 'BYDAY': '1SU', 'BYMONTH': 11})
+
+timezone.add_component(standard)
+
+daylight = icalendar.TimezoneDaylight()
+daylight.add('DTSTART', datetime(1970, 3, 10, 2, 0, 0))
+daylight['TZOFFSETFROM'] =  '-0800'
+daylight['TZOFFSETTO'] =  '-0700'
+daylight.add('TZNAME', 'PDT')
+daylight.add('RRULE', {'FREQ': 'YEARLY', 'BYDAY': '2SU', 'BYMONTH': 3})
+
+
+timezone.add_component(daylight)
+c.add_component(timezone)
+# each course has a: course code & number, type, days, time, location, start, and end
 
 #print(excel.iloc[0,4])
 
@@ -71,8 +112,6 @@ for x in range(0, excel.shape[0]):
     e = icalendar.Event()
 
     e.add('summary', course_name)
-    e.add('version', 2.0)
-    e.add('proid', "myCalender")
 
     #e.name = course_name
 
@@ -81,7 +120,6 @@ for x in range(0, excel.shape[0]):
     else:
         start_date = start_date.replace(day= (start_date.day + start_date_modifer[days[0]]) )
 
-    tz = dateutil.tz.tzstr("Canada/Vancouver")
     e["X-DT-ZONEINFO"] = icalendar.vDatetime(datetime(start_date.year, start_date.month, start_date.day, start_time.hour, start_time.minute, tzinfo=tz))
     e.add("dtstart", icalendar.vDatetime(datetime(start_date.year, start_date.month, start_date.day, start_time.hour, start_time.minute, tzinfo=tz)))
     #e.add("dtend", datetime(date_obj.year, date_obj.month, date_obj.day, end_time.hour, end_time.minute))
@@ -100,7 +138,9 @@ for x in range(0, excel.shape[0]):
     recurrance = icalendar.vRecur({
         'freq': 'weekly',
         'byday': d,
-        'until': end_date
+        'until': end_date,
+        'wkst': 'mo',
+        'interval': 1
     })
 
     e.add('rrule', recurrance)
